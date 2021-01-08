62395853c079f36315d6a89d840c95a3a6b42ef3
sureshnaloor <suresh.naloor@gmail.com>
2021-01-08 19:11:53 +0300

    projectname and incharge query add in project route

--- serverapp/controllers/projects.controller.js
+++ serverapp/controllers/projects.controller.js
@@ -4,10 +4,24 @@ const Project = db.projects;
 //retrieve all PROJECTS from database, if project-name  search box is filled in search by description
 exports.findAll = (req, res) => {
 	const projectName = req.query.projectName;
+	const projectIncharge = req.query.projectIncharge;
 
 	const noRec = 100;
-	var condition = projectName
-		? { 'project-name': { $regex: new RegExp(projectName), $options: 'i' } }
+
+	var condition = projectIncharge
+		? {
+				'project-incharge': {
+					$regex: new RegExp(projectIncharge),
+					$options: 'i',
+				},
+		  }
+		: projectName
+		? {
+				'project-name': {
+					$regex: new RegExp(projectName),
+					$options: 'i',
+				},
+		  }
 		: {};
 
 	Project.find(condition)
